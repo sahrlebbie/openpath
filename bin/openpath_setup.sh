7c2629871334b693217788e4beded6352f7c04f3
Sahr Lebbie <42525760+sahrlebbie@users.noreply.github.com>
2019-12-29 18:35:56 -0500

    Update openpath_setup.sh

--- bin/openpath_setup.sh
+++ bin/openpath_setup.sh
@@ -3,7 +3,6 @@
 clear
 #Lets install some network tools to make sure we can download and run networking/troubleshooting steps.
 echo "#Lets install some network tools to make sure we can download and run networking/troubleshooting steps."
-
 sleep 3s
 sudo yum install -y tcpdump telnet wget
 sudo yum install -y epel-release
@@ -18,8 +17,11 @@ sleep 4s
 
 echo "This is OpenPath's AWS User Data Script. It is designed to add the splunk user at boot."
 sudo useradd splunk
+clear
+echo "Let's set the splunk service account password"
+sleep 2s
 sudo passwd splunk
-usermod -Ag wheel splunk
+usermod -aG wheel splunk
 cd /opt
 sleep 3s
 clear
@@ -28,21 +30,28 @@ echo "You should have a link from your Cloud Admin which references what Package
 sleep 2s
 echo "Please paste the link now:"
 read AWS_LINK
+sleep 1s
 
 #We will save this file as splunk_package.tar since it's essentially a tar file we are dowloading from AWS S3
 curl $AWS_LINK > /opt/splunk_package.tar
 tar -xvf /opt/splunk_package.tar
-sudo /opt/splunk/bin/splunk enable boot-start -user splunk
 echo "OK, Splunk is now installed..."
+sleep 2s
+
+echo "We will now start Splunk and then install the openPath app"
+sudo chown -R splunk:splunk /opt/splunk
+sudo -u splunk /opt/splunk/bin/splunk start --accept-license
+sudo -u splunk /opt/splunk/bin/splunk enable boot-start -user splunk
 
 #Now we will instlall the app from GitHub. The directory is saved as openpath in GitHub
-sleep 4s
+sleep 2s
 echo "We will pull down the openPath app from OpenPath's GitHub Page"
+sleep 4s
 GITHUB_APP="https://github.com/sahrlebbie/openpath.git"
 sudo git clone $GITHUB_APP
 
 sudo mv /opt/openpath /opt/splunk/etc/apps
 sudo chown -R splunk:splunk /opt/splunk
 
-sudo systemctl start splunk
-sudo systemctl status splunk
+sudo -u splunk /opt/splunk/bin/splunk start
+sudo -u splunk /opt/splunk/bin/splunk status
