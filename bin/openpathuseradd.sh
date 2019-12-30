979738ce342432217e83161b911585674d9cb29b
Sahr Lebbie <42525760+sahrlebbie@users.noreply.github.com>
2019-12-29 23:06:31 -0500

    Update openpathuseradd.sh

--- bin/openpathuseradd.sh
+++ bin/openpathuseradd.sh
@@ -2,7 +2,7 @@
 #This script will look to find all the users inside of the users.conf file located in /opt/splunk/etc/apps/openpath/default/user.conf.
 #It will then attempt to add each user to this instance with a for loop.
 
-for USER in $(cat /opt/splunk/etc/apps/openpath/default/user.conf)
+for USER in $(cat /opt/splunk/etc/apps/openpath/default/users.conf)
 do
 /opt/splunk/bin/splunk add user $USER -password Splunk18! -role admin -auth "admin:KrBpqtHnRpswx$$6mQEyzkdrWn!"
 echo "The following user has been added to OpenPath Prod: $USER"
